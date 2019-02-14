7fcc69ccbccd40b606b67b237bb90cfb682113e7
Zeldarck <zeldarck.martin@gmail.com>
2019-02-14 22:04:09 +0100

    Basic movement

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    GameObject m_objectToFollow;
+
+    // TODO change how this is fixed/control and orientation
+    [SerializeField]
+    Vector3 m_offset;
+
+    [SerializeField]
+    Vector3 m_orientation;
+
+    [SerializeField]
+    float m_speed;
+
+
+    #region getter/setter
+    public GameObject ObjectToFollow
+    {
+        get
+        {
+            return m_objectToFollow;
+        }
+
+        set
+        {
+            m_objectToFollow = value;
+        }
+    }
+
+    public Vector3 Offset
+    {
+        get
+        {
+            return m_offset;
+        }
+
+        set
+        {
+            m_offset = value;
+        }
+    }
+    #endregion //getter/setter
+
+
+    void Update()
+    {
+        if (ObjectToFollow)
+        {
+            transform.position = ObjectToFollow.transform.position + Offset;
+            transform.localRotation = Quaternion.Euler(m_orientation);
+
+            if (Input.GetAxis("Mouse ScrollWheel") > 0f) // forward
+            {
+                Offset += gameObject.transform.forward * m_speed;
+            }
+            else if (Input.GetAxis("Mouse ScrollWheel") < 0f) // backwards
+            {
+                Offset -= gameObject.transform.forward * m_speed;
+            }
+
+        }
+
+    }
+
+}
+
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Networking;
+
+
+
+public class PlayerController : MonoBehaviour
+{
+
+    NavMeshAgent agent;
+
+    [SerializeField]
+    float m_speed;
+
+
+    void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        Camera.main.GetComponent<CameraFollow>().ObjectToFollow = gameObject;
+        //agent.speed = m_speed;   
+    }
+
+    void Update () {
+
+        bool isMoving = false;
+        Vector3 target = transform.position;
+
+        if (Input.GetKey(KeyCode.Z))
+        {
+            target += gameObject.transform.forward * m_speed;
+            isMoving = true;
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            target += gameObject.transform.right * -1 * m_speed;
+            isMoving = true;
+        }
+       /* if (Input.GetKey(KeyCode.S))
+        {
+            target += gameObject.transform.forward * -1 * m_speed;
+            isMoving = true;
+        }*/
+        if (Input.GetKey(KeyCode.D))
+        {
+            target += gameObject.transform.right * m_speed;
+            isMoving = true;
+        }
+
+        if (isMoving)
+        {
+            agent.ResetPath();
+        }
+
+        if (Input.GetButtonDown("Fire2") && !isMoving)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                target = hit.point;
+                isMoving = true;
+            }
+        }
+
+
+        if (isMoving)
+        {
+            agent.SetDestination(target);
+        }
+
+
+    }
+}
