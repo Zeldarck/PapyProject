7541a46948b66c759690b4c0e2bd70040d0a2c6e
Zeldarck <zeldarck.martin@gmail.com>
2019-03-22 23:10:01 +0100

    CondSys - set of conditions

--- Assets/Scripts/Lib/ConditionSystem/Condition.cs
+++ Assets/Scripts/Lib/ConditionSystem/Condition.cs
@@ -5,6 +5,56 @@ using System.Reflection;
 using System.Linq;
 using System;
 
+
+
+[Serializable]
+public class ConditionSet
+{
+    [SerializeField]
+    List<ConditionCollection> m_conditions;
+
+
+    public bool Execute()
+    {
+        bool res = false;
+        foreach (ConditionCollection conditionCollec in m_conditions)
+        {
+            res = conditionCollec.Execute();
+            if (res)
+            {
+                break;
+            }
+        }
+
+        return res;
+    }
+}
+
+
+[Serializable]
+public class ConditionCollection
+{
+    [SerializeField]
+    List<Condition> m_conditions;
+
+
+    public bool Execute()
+    {
+        bool res = true;
+        foreach (Condition condition in m_conditions)
+        {
+            res = condition.Execute();
+            if (!res)
+            {
+                break;
+            }
+        }
+
+        return res;
+    }
+}
+
+
 [Serializable]
 public class Condition
 {
@@ -22,3 +72,5 @@ public class Condition
     }
 
 }
+
+
